bc4d9173abea8305377778e640bec6b11ef814f7
Tobias Krojer <tobias.krojer@maxiv.lu.se>
2022-03-18 10:09:52 +0100

    initial commit

--- run_initial_refinement.py
+++ run_initial_refinement.py
@@ -23,43 +23,105 @@ import glob
 import sys
 import os
 import gemmi
+from tabulate import tabulate
 
 
-def create_sample_folder_in_project_dir(projectDir, sample):
-    os.chdir(projectDir)
-    if not os.path.isdir(sample):
-        os.mkdir(sample)
+def analyse_process_directory(sample_folder, pdbDir):
+    table = []
+    header = ['run', 'process', 'resolution', 'spacegroup', 'reference_pdb', 'delta(UCvolume)']
+    pipelines = ['autoproc', 'staraniso', 'dials']
+    for runs in sorted(glob.glob(os.path.join(sample_folder, '*'))):
+        run = runs.split('/')[len(runs.split('/'))-1]
+        for process_pipeline in pipelines:
+            pipeline, mtz_extension, log_extension, cif_extension = get_pipeline_path(process_pipeline)
+            for mtzfile in sorted(glob.glob(os.path.join(runs, pipeline))):
+                mtz = mtz_info(mtzfile)
+                pdb, diff, pdb_name = find_pdb_input_file(pdbDir, mtzfile)
+                table.append([run, process_pipeline, round(float(mtz['resolution_high']), 2), mtz['space_group'], pdb_name, diff])
+    print(tabulate(table, headers=header))
+    print("\n")
 
+
+def print_summary():
+    print('\n')
+    print('Number of samples: ')
+    print()
+
+
+def create_sample_folder_in_project_dir(projectDir, sample, analyseOnly):
+    if not analyseOnly:
+        os.chdir(projectDir)
+        if not os.path.isdir(sample):
+            os.mkdir(sample)
+
+def get_pipeline_path(process_pipeline):
+
+
+    # /data/visitors/biomax/20200054/20211113/process/hsDHS/hsDHS-x0139/xds_hsDHS-x0139_2_1/autoPROC/cn83_20211114-175135/AutoPROCv1_0_anom/Data_2_autoPROC_TRUNCATE_all.cif
+    # /data/visitors/biomax/20200054/20211113/process/hsDHS/hsDHS-x0139/xds_hsDHS-x0139_2_1/autoPROC/cn83_20211114-175135/AutoPROCv1_0_anom/HDF5_1/truncate-unique.mtz
+    # /data/visitors/biomax/20200054/20211113/process/hsDHS/hsDHS-x0139/xds_hsDHS-x0139_2_1/autoPROC/cn83_20211114-175135/AutoPROCv1_0_anom/HDF5_1/aimless.log
+
+    # /data/visitors/biomax/20200054/20211113/process/hsDHS/hsDHS-x0139/xds_hsDHS-x0139_2_1/autoPROC/cn83_20211114-175135/AutoPROCv1_0_anom/Data_1_autoPROC_STARANISO_all.cif
+    # /data/visitors/biomax/20200054/20211113/process/hsDHS/hsDHS-x0139/xds_hsDHS-x0139_2_1/autoPROC/cn83_20211114-175135/AutoPROCv1_0_anom/HDF5_1/staraniso_alldata-unique.mtz
+    # /data/visitors/biomax/20200054/20211113/process/hsDHS/hsDHS-x0139/xds_hsDHS-x0139_2_1/autoPROC/cn83_20211114-175135/AutoPROCv1_0_anom/HDF5_1/staraniso_alldata.log
+
+    if process_pipeline == 'autoproc':
+        pipeline = os.path.join('autoPROC', 'cn*', 'AutoPROCv1_*', 'HDF5_1', 'truncate-unique.mtz')
+        mtz_extension = 'HDF5_1/truncate-unique.mtz'
+        log_extension = 'HDF5_1/aimless.log'
+        cif_extension = 'Data_2_autoPROC_TRUNCATE_all.cif'
+    elif process_pipeline == 'staraniso':
+        pipeline = os.path.join('autoPROC', 'cn*', 'AutoPROCv1_*', 'HDF5_1', 'staraniso_alldata-unique.mtz')
+        mtz_extension = 'HDF5_1/staraniso_alldata-unique.mtz'
+        log_extension = 'HDF5_1/staraniso_alldata.log'
+        cif_extension = 'Data_1_autoPROC_STARANISO_all.cif'
+    elif process_pipeline == 'dials':
+        # /data/visitors/biomax/20200054/20211113/process/hsDHS/hsDHS-x0139/xds_hsDHS-x0139_2_1/xia2DIALS/cn68_20211114-175326/Xia2DIALSv1_0_anom/DataFiles/xia2.mmcif.bz2
+        # /data/visitors/biomax/20200054/20211113/process/hsDHS/hsDHS-x0139/xds_hsDHS-x0139_2_1/xia2DIALS/cn68_20211114-175326/Xia2DIALSv1_0_anom/DataFiles/AUTOMATIC_DEFAULT_free.mtz
+        # /data/visitors/biomax/20200054/20211113/process/hsDHS/hsDHS-x0139/xds_hsDHS-x0139_2_1/xia2DIALS/cn68_20211114-175326/Xia2DIALSv1_0_anom/LogFiles/AUTOMATIC_DEFAULT_SCALE.log
+        pipeline = os.path.join('xia2DIALS', 'cn*', 'Xia2DIALSv1_*', 'DataFiles', 'AUTOMATIC_DEFAULT_free.mtz')
+#        print(pipeline)
+        mtz_extension = 'DataFiles/AUTOMATIC_DEFAULT_free.mtz'
+        log_extension = 'LogFiles/AUTOMATIC_DEFAULT_SCALE.log'
+        cif_extension = 'DataFiles/xia2.mmcif.bz2'
+    return pipeline, mtz_extension, log_extension, cif_extension
 
 def find_autoproc_results(sample_folder, process_pipeline):
     bestmtz = None
     bestlog = None
-    if process_pipeline == 'autoproc':
-        pipeline = os.path.join('autoPROC', 'results', '*_anom_truncate.mtz')
-        log = '-unique.table1'
+    bestcif = None
+    pipeline, mtz_extension, log_extension, cif_extension = get_pipeline_path(process_pipeline)
     try:
         resoList = []
+#        print('mtzfile', os.path.join(sample_folder, '*', pipeline))
         for mtzfile in glob.glob(os.path.join(sample_folder, '*', pipeline)):
-            if os.path.isfile(mtzfile.replace('.mtz', log)):
-                logfile = mtzfile.replace('.mtz', log)
+            if os.path.isfile(mtzfile.replace(mtz_extension, log_extension)):
+                logfile = mtzfile.replace(mtz_extension, log_extension)
+            if os.path.isfile(mtzfile.replace(mtz_extension, cif_extension)):
+                ciffile = mtzfile.replace(mtz_extension, cif_extension)
             else:
                 logfile = None
-            mtz = gemmi.read_mtz_file(mtzfile)
-            resoList.append([mtzfile, float(mtz.resolution_high()), logfile])
+#            mtz = gemmi.read_mtz_file(mtzfile)
+            mtz = mtz_info(mtzfile)
+#            print('>>>>>>>>>', mtz['unitcell'])
+            resoList.append([mtzfile, float(mtz['resolution_high']), logfile, ciffile])
         if resoList:
             bestmtz = min(resoList, key=lambda x: x[1])[0]
             bestlog = min(resoList, key=lambda x: x[1])[2]
+            bestcif = min(resoList, key=lambda x: x[1])[3]
     except UnboundLocalError:
         pass
-    return bestmtz, bestlog
+    return bestmtz, bestlog, bestcif
 
 
-def link_files_to_project_folder(processDir, sample, mtz, log):
-    os.chdir(processDir, sample)
+def link_files_to_project_folder(projectDir, sample, mtz, log, cif):
+    os.chdir(os.path.join(projectDir, sample))
     if not os.path.isfile('process.mtz'):
-        print('ln -s {0!s} process.mtz'.format(mtz))
+        os.system('ln -s {0!s} process.mtz'.format(mtz))
     if not os.path.isfile('process.log'):
-        print('ln -s {0!s} process.log'.format(log))
+        os.system('ln -s {0!s} process.log'.format(log))
+    if not os.path.isfile('process.cif'):
+        os.system('ln -s {0!s} process.cif'.format(cif))
 
 
 def mtz_point_group_uc_volume(mtzfile):
@@ -70,11 +132,22 @@ def mtz_point_group_uc_volume(mtzfile):
     point_group = sym.point_group_hm()
     return point_group, unitcell_volume
 
+def mtz_info(mtzfile):
+    mtzDict = {}
+    mtz = gemmi.read_mtz_file(mtzfile)
+    mtzDict['unitcell'] = mtz.cell
+    mtzDict['unitcell_volume'] = mtz.cell.volume
+    mtzDict['point_group'] = mtz.spacegroup.point_group_hm()
+    mtzDict['resolution_high'] = mtz.resolution_high()
+    mtzDict['space_group'] = mtz.spacegroup.hm
+    return mtzDict
+
 
 def pdb_point_group_uc_volume(pdbfile):
     structure = gemmi.read_pdb(pdbfile)
+    unitcell = structure.cell
     unitcell_volume = unitcell.volume
-    structure.spacegroup_hm
+#    structure.spacegroup_hm
     sym = gemmi.find_spacegroup_by_name(structure.spacegroup_hm)
     point_group = sym.point_group_hm()
     return point_group, unitcell_volume
@@ -82,17 +155,19 @@ def pdb_point_group_uc_volume(pdbfile):
 
 def find_pdb_input_file(pdbDir, mtz):
     pdb = None
+    pdb_name = None
     mtz_point_group, mtz_unitcell_volume = mtz_point_group_uc_volume(mtz)
     for pdbfile in glob.glob(os.path.join(pdbDir, '*.pdb')):
         pdb_point_group, pdb_unitcell_volume = pdb_point_group_uc_volume(pdbfile)
         diff = abs(mtz_unitcell_volume - pdb_unitcell_volume) / pdb_unitcell_volume
         if mtz_point_group == pdb_point_group and diff < 0.1:
             pdb = pdbfile
-    return pdb
+            pdb_name = pdb.split('/')[len(pdb.split('/'))-1]
+    return pdb, diff, pdb_name
 
 
-def prepare_init_refine_script(processDir, sample, pdb, refine_pipeline):
-    os.chdir(os.path.join(processDir, sample))
+def prepare_init_refine_script(projectDir, sample, pdb, refine_pipeline):
+    os.chdir(os.path.join(projectDir, sample))
     cmd = (
             '#!/bin/bash\n'
             '#SBATCH --time=10:00:00\n'
@@ -103,31 +178,42 @@ def prepare_init_refine_script(processDir, sample, pdb, refine_pipeline):
     if refine_pipeline == 'dimple':
         cmd += (
                 'module load gopresto CCP4/7.1.016-SHELX-ARP-8.0-1-PReSTO\n'
-                'cd {0!s}\n'.format(os.path.join(processDir, sample)) +
+                'cd {0!s}\n'.format(os.path.join(projectDir, sample)) +
                 'dimple process.mtz {0!s} dimple'.format(pdb)
         )
 
-    print(cmd)
-#    f = open('{0!s}.sh'.format(refine_pipeline), 'w')
-#    f.write(cmd)
-#    f.close()
+    elif refine_pipeline == 'pipedream':
+        cmd += (
+        		'module load gopresto BUSTER\n'
+        		'cd {0!s}\n'.format(os.path.join(projectDir, sample)) +
+        		'pipedream -hklin process.mtz -xyzin {0!s} -d pipedream -nofreeref -nolmr'.format(pdb)
+        )
 
-def submit_init_refine_script(processDir, sample, refine_pipeline):
-    os.chdir(os.path.join(processDir, sample))
+    f = open('{0!s}.sh'.format(refine_pipeline), 'w')
+    f.write(cmd)
+    f.close()
+
+def submit_init_refine_script(projectDir, sample, refine_pipeline):
+    os.chdir(os.path.join(projectDir, sample))
     print('submitting {0!s} job for {1!s}'.format(refine_pipeline, sample))
     print('sbatch {0!s}.sh'.format(refine_pipeline))
 
 
-def run_initial_refinement(processDir, projectDir, pdbDir, process_pipeline, refine_pipeline):
-    for sample_folder in sorted(glob.glob(os.path.join(processDir, '*'))):
+def run_initial_refinement(processDir, projectDir, pdbDir, process_pipeline, refine_pipeline, analyseOnly):
+    for n, sample_folder in enumerate(sorted(glob.glob(os.path.join(processDir, '*')))):
         sample = sample_folder.split('/')[len(sample_folder.split('/'))-1]
-        print(sample)
-        create_sample_folder_in_project_dir(projectDir, sample)
-        mtz, log = find_autoproc_results(sample_folder, process_pipeline)
-        link_files_to_project_folder(processDir, sample, mtz, log)
-        pdb = find_pdb_input_file(pdbDir, mtz)
-        prepare_init_refine_script(processDir, sample, pdb, refine_pipeline)
-        submit_init_refine_script(processDir, sample, refine_pipeline)
+        print('sample: ' + sample + '\n')
+        if analyseOnly:
+            analyse_process_directory(sample_folder, pdbDir)
+        else:
+#            create_sample_folder_in_project_dir(projectDir, sample, analyseOnly)
+            mtz, log, cif = find_autoproc_results(sample_folder, process_pipeline)
+            if mtz:
+                create_sample_folder_in_project_dir(projectDir, sample, analyseOnly)
+                link_files_to_project_folder(projectDir, sample, mtz, log, cif)
+                pdb, diff, pdb_name = find_pdb_input_file(pdbDir, mtz)
+                prepare_init_refine_script(projectDir, sample, pdb, refine_pipeline)
+                submit_init_refine_script(projectDir, sample, refine_pipeline)
 
 
 
@@ -151,6 +237,8 @@ def usage():
         '    initial refinement pipeline (e.g. dimple)\n'
         '--overwrite, -o\n'
         '    flag to overwrite files\n'
+        '--analyse, -y\n'
+        '    flag to analyse process directory, without file operations\n'
     )
     print(usage)
 
@@ -163,10 +251,11 @@ def main(argv):
     process_pipeline = None
     refine_pipeline = None
     overwrite = False
+    analyseOnly = False
 
     try:
-        opts, args = getopt.getopt(argv,"i:o:p:a:r:h",["input=", "output=", "pdbdir=",
-                                                         "autoproc=", "refine=",])
+        opts, args = getopt.getopt(argv,"i:o:p:a:r:hy",["input=", "output=", "pdbdir=",
+                                                         "autoproc=", "refine=", "analyse"])
     except getopt.GetoptError:
         print('foehfuie')
 #        usage()
@@ -186,10 +275,12 @@ def main(argv):
             process_pipeline = arg
         elif opt in ("-r", "--refine"):
             refine_pipeline = arg
+        elif opt in ("-y", "--analyse"):
+            analyseOnly = True
 
 
 #    try:
-    run_initial_refinement(processDir, projectDir, pdbDir, process_pipeline, refine_pipeline)
+    run_initial_refinement(processDir, projectDir, pdbDir, process_pipeline, refine_pipeline, analyseOnly)
 #    except TypeError:
 #        print('kkkkkk')
 #        usage()
